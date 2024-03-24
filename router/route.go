2d5b6e425fd51a7c4cf9c788664e01753d88e873
NicolasRampoldi <58613770+NicolasRampoldi@users.noreply.github.com>
2024-03-24 12:59:35 -0300

    feat: add cors middleware

--- router/route.go
+++ router/route.go
@@ -3,18 +3,20 @@ package router
 import (
 	"yab-explorer/configs"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Init(init *configs.Initialization) *gin.Engine {
 
 	router := gin.New()
+	router.Use(cors.Default())
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
 
 	orders := router.Group("/orders")
 	{
-		orders.GET("/", init.OrderController.GetOrders)
+		orders.GET("", init.OrderController.GetOrders)
 
 		orders.GET("/:orderID", init.OrderController.GetOrder)
 	}
