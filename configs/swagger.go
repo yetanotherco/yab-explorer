5c9818c62252d48171c3570c00086f69997f96b9
NicolasRampoldi <58613770+NicolasRampoldi@users.noreply.github.com>
2024-04-03 12:31:32 -0300

    refactor: change swagger host

--- configs/swagger.go
+++ configs/swagger.go
@@ -8,7 +8,7 @@ func InitSwagger(apiPort string) {
 	docs.SwaggerInfo.Title = "YAB Explorer API"
 	docs.SwaggerInfo.Description = "YAB Explorer API"
 	docs.SwaggerInfo.Version = "1.0"
-	docs.SwaggerInfo.Host = "localhost:" + apiPort
+	docs.SwaggerInfo.Host = "www.explorer.yetanotherbridge.com/"
 	docs.SwaggerInfo.BasePath = ""
-	docs.SwaggerInfo.Schemes = []string{"http"}
+	docs.SwaggerInfo.Schemes = []string{"https"}
 }
